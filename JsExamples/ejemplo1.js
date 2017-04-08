9722ef97b81acfc6a22f312d6cd9963bb194957f
jmartinezra <ramirez_0157@hotmail.com>
2017-04-08 10:35:27 -0500

    First Commit

---
+++ JsExamples/ejemplo1.js
@@ -0,0 +1,6 @@
+function msg() {
+  var text1 = document.getElementById("textbox1").value;
+  var text2 = document.getElementById("textbox2").value;
+  var suma = Number(text1) + Number(text2);
+  alert(suma);
+}
